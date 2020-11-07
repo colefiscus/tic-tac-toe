38f4e2b5819e0db539e454a404c4a736107ec218
Cole Fiscus <coletfiscus@gmail.com>
2020-11-06 18:37:46 -0700

    Add JS files and state to player and game

---
+++ src/game.js
@@ -0,0 +1,6 @@
+class Game {
+  constructor(playerObject1, playerObject2) {
+    this.player1 = playerObject1;
+    this.player2 = playerObject2;
+  };
+};
---
+++ src/player.js
@@ -0,0 +1,8 @@
+class Player {
+  constructor(id, token, turn) {
+    this.id = id;
+    this.token = token;
+    this.wins = 0;
+    this.myTurn = turn
+  };
+};
